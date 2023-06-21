a172d8febc9a891373f5f780aeaeb42a383cd55c
Brandimic <87997719+Brandimic0@users.noreply.github.com>
2023-06-22 02:21:00 +0300

    Revert "fix: sort options in group by field"

--- frappe/public/js/frappe/ui/group_by/group_by.js
+++ frappe/public/js/frappe/ui/group_by/group_by.js
@@ -324,9 +324,9 @@ frappe.ui.GroupBy = class {
 			);
 
 			if (this.aggregate_function === "sum") {
-				docfield.label = __("Sum of {0}", [__(docfield.label)]);
+				docfield.label = __("Sum of {0}", [docfield.label]);
 			} else {
-				docfield.label = __("Average of {0}", [__(docfield.label)]);
+				docfield.label = __("Average of {0}", [docfield.label]);
 			}
 		}
 
@@ -367,9 +367,7 @@ frappe.ui.GroupBy = class {
 			["Select", "Link", "Data", "Int", "Check"].includes(f.fieldtype)
 		);
 		const tag_field = { fieldname: "_user_tags", fieldtype: "Data", label: __("Tags") };
-		this.group_by_fields[this.doctype] = fields
-			.concat(tag_field)
-			.sort((a, b) => __(a.label).localeCompare(__(b.label)));
+		this.group_by_fields[this.doctype] = fields.concat(tag_field);
 		this.all_fields[this.doctype] = this.report_view.meta.fields;
 
 		const standard_fields_filter = (df) =>
@@ -381,8 +379,7 @@ frappe.ui.GroupBy = class {
 			const cdt = df.options;
 			const child_table_fields = frappe.meta
 				.get_docfields(cdt)
-				.filter(standard_fields_filter)
-				.sort((a, b) => __(a.label).localeCompare(__(b.label)));
+				.filter(standard_fields_filter);
 			this.group_by_fields[cdt] = child_table_fields;
 			this.all_fields[cdt] = child_table_fields;
 		});
